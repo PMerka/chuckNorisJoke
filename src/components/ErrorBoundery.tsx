e7636f6836cbff346d3f9ede9897506b15829bd4
PMerka <pavelmerka1995@gmail.com>
2025-10-05 20:20:10 +0200

    update favicon remove unused assets

--- src/components/ErrorBoundery.tsx
+++ src/components/ErrorBoundery.tsx
@@ -26,7 +26,6 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
 
   render() {
     if (this.state.hasError) {
-      // fallback z props nebo default
       return this.props.fallback ?? <h1>Something went wrong.</h1>;
     }
     return this.props.children;
